f07e60494cd15f90dc5992470d370d223c0039e5
haru2036 <haru2036@gmail.com>
2019-07-19 17:55:10 +0900

    登録済みでないと401を返すようにした

--- src/Api/Sleep.hs
+++ src/Api/Sleep.hs
@@ -5,18 +5,22 @@ import Database.Persist.Sql
 import Database.Persist.Class
 import Control.Monad.IO.Class
 import DataStore.Internal
+import Api.Common
 
 getSleeps :: ConnectionPool -> User -> Handler [ClientSleep]
-getSleeps pool user = liftIO $ flip runSqlPool pool $ do
-    list <- selectList [SleepUser ==. user] [Desc SleepStart]
-    return $ map (toClientSleep . entityVal) list
+getSleeps pool user = 
+    doIfRegistered pool user $ return $ liftIO $ flip runSqlPool pool $ do
+        list <- selectList [SleepUser ==. user] [Desc SleepStart]
+        return $ map (toClientSleep . entityVal) list
     
 
 postSleeps :: ConnectionPool -> User -> [ClientSleep] -> Handler [ClientSleep]
-postSleeps pool user sleeps = liftIO $ flip runSqlPool pool $ do
-    _ <- putMany $ map (fromClientSleep user) sleeps
-    list <- selectList [SleepUser ==. user] [Desc SleepStart]
-    return $ map (toClientSleep . entityVal) list
+postSleeps pool user sleeps = 
+    doIfRegistered pool user $ return $ liftIO $ flip runSqlPool pool $ do
+        _ <- putMany $ map (fromClientSleep user) sleeps
+        list <- selectList [SleepUser ==. user] [Desc SleepStart]
+        return $ map (toClientSleep . entityVal) list
+
 
 toClientSleep :: Sleep -> ClientSleep
 toClientSleep s = ClientSleep (sleepStart s) (sleepEnd s)
